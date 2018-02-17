cf3132b6233a51f38199df9d70d79d58ca585bd8
ingthorb <ingthor@travelade.com>
2018-02-17 22:04:37 +0000

    Finished with post routes, next up delete and modify

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const { Pool } = require('pg');
 const pool = new Pool();
+var contentType = "application/json";
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 /* GET All regions */
 router.get('/regions', function(req, res, next) {
@@ -48,67 +51,126 @@ router.get('/fields', function(req, res, next) {
 
 /* POST region */
 router.post('/regions', function(req, res, next) {
-  console.log(req.body.countrypart);
-  pool.query('SELECT countrypart WHERE countrypart = $1 FROM region ORDER BY countrypart ASC', [req.body.countrypart], (err, resp) => {
-    if(err) {
-      console.log(err.message);
-      res.status(err.status);
+  if(req.get('Content-Type') !== contentType)
+  {
+    res.statusCode = 415;
+    return res.json("Unsupported Media Type");
+  }
+
+  if(req.body.countrypart == undefined)
+  {
+    res.status(400);
+    res.send("Not correct format, specify countrypart");
+  }
+
+  pool.query('SELECT countrypart FROM region WHERE countrypart = $1', [req.body.countrypart], (err, resp) => {
+    if(resp) {
+      if(resp.rows.length !== 0)
+      {
+        res.status(400);
+        res.send("Duplicate entry in database");
+      }
+      else {
+        pool.query('INSERT INTO region(countrypart) VALUES($1)', [req.body.countrypart], (err, resp) => {
+          if(err) {
+            console.log(err.message);
+            res.status(err.status);
+          }
+          else {
+            res.status(201);
+            res.send(resp.rows);
+          }
+        });
+      }
     }
     else {
-      pool.query('INSERT INTO region(countrypart) VALUES($1)', [req.body.countrypart], (err, resp) => {
-        if(err) {
-          console.log(err.message);
-          res.status(err.status);
-        }
-        else {
-          res.status(201);
-          res.send(resp.rows);
-        }
-      });
+      res.status(201);
+      res.send(resp.rows);
       }
   });
 });
 
 /* POST Town */
 router.post('/towns', function(req, res, next) {
-  pool.query('SELECT cityname WHERE cityname = $1 FROM towns ORDER BY countrypart ASC', [req.body.cityname], (err, resp) => {
-    if(err) {
-      console.log(err.message);
-      res.status(err.status);
+  console.log(req.body.cityname);
+  console.log(req.body.countrypart);
+  if(req.get('Content-Type') !== contentType)
+  {
+    res.statusCode = 415;
+    return res.json("Unsupported Media Type");
+  }
+
+  if(req.body.cityname == undefined || req.body.countrypart == undefined)
+  {
+    res.status(400);
+    res.send("Not correct format, specify countrypart and cityname");
+  }
+
+  pool.query('SELECT cityname FROM towns WHERE countrypart = $1 cityname = $2', [req.body.countrypart, req.body.cityname], (err, resp) => {
+    if(resp) {
+      if(resp.rows.length !== 0)
+      {
+        res.status(400);
+        res.send("Duplicate entry in database");
+      }
+      else {
+        pool.query('INSERT INTO towns(countrypart,cityname) VALUES($1, $2)', [req.body.countrypart, req.body.cityname], (err, resp) => {
+          if(err) {
+            console.log(err.message);
+            res.status(err.status);
+          }
+          else {
+            res.status(201);
+            res.send(resp.rows);
+          }
+        });
+      }
     }
     else {
-      pool.query('INSERT INTO towns(countrypart,cityname) VALUES($1, $2)', [req.body.countrypart, req.body.cityname], (err, resp) => {
-        if(err) {
-          console.log(err.message);
-          res.status(err.status);
-        }
-        else {
-          res.status(201);
-          res.send(resp.rows);
-        }
-      });
+      console.log(err.message);
+      res.status(err.status);
     }
   });
 });
 
 /* POST field */
 router.post('/fields', function(req, res, next) {
-  pool.query('SELECT cityname WHERE cityname = $1 FROM fields ORDER BY cityname ASC', [req.body.cityname], (err, resp) => {
-    if(err) {
-      console.log(err.message);
-      res.status(err.status);
+  if(req.get('Content-Type') !== contentType)
+  {
+    res.statusCode = 415;
+    return res.json("Unsupported Media Type");
+  }
+
+  if(req.body.lat == undefined || req.body.countrypart == undefined || req.body.lng == undefined || req.body.photos == undefined || req.body.rating == undefined)
+  {
+    res.status(400);
+    res.send("Not correct format, specify countrypart, lat, lng, photo and rating");
+  }
+
+  pool.query('SELECT cityname FROM fields WHERE lat = $1 AND cityname = $2', [req.body.lat, req.body.cityname], (err, resp) => {
+    if(resp) {
+      console.log(resp);
+      if(resp.rows.length !== 0)
+      {
+        res.status(400);
+        res.send("Duplicate entry in database");
+      }
+      else {
+        pool.query('INSERT INTO fields(cityname, lat, lng, photos, rating) VALUES($1, $2, $3, $4, $5)', [req.body.cityname, req.body.lat, req.body.lng, req.body.photos, req.body.rating], (err, resp) => {
+          if(err) {
+            console.log(err.message);
+            res.status(err.status);
+          }
+          else {
+            res.status(201);
+            res.send(resp.rows);
+          }
+        });
+      }
     }
     else {
-      pool.query('INSERT INTO fields(cityname, lat, lng, photos, rating) VALUES($1, $2, $3, $4, $5)', [req.body.cityname, req.body.lat, req.body.lng, req.body.photos, req.body.rating], (err, resp) => {
-        if(err) {
-          console.log(err.message);
-          res.status(err.status);
-        }
-        else {
-          res.status(201);
-          res.send(resp.rows);
-        }
-      });
+      console.log(err.message);
+      res.status(err.status);
     }
   });
 });
